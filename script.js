8441aa2de658c9a4d043d43eee665a0de200440a
Sidharth <76509525+SIDDHU123M@users.noreply.github.com>
2024-10-17 18:48:51 +0000

    fixed structure and urls

--- script.js
+++ script.js
@@ -23,43 +23,6 @@ if (typeof window !== "undefined") {
     }
 }
 
-// Set GitHub link dynamically
-
-// Use the native fetch API for browser environments
-fetch("json/main.json")
-    .then((response) => response.json())
-    .then((data) => {
-        const githubLinkElement = document.getElementById("githubLink");
-        githubLinkElement.href = data.githubLink;
-		githubLinkElement.target = "_blank";
-    })
-    .catch((error) => console.error("Error fetching GitHub link:", error));
-
-// // Dynamic List Script
-// const fetchDataAndCreateLinks = (skills, containerId) => {
-// 	fetch("json/main.json")
-// 		.then((response) => response.json())
-// 		.then((data) => {
-// 			const skillData = data[skills];
-// 			const container = document.getElementById(containerId);
-// 			for (const key in skillData) {
-// 				if (skillData.hasOwnProperty(key)) {
-// 					const linkItem = document.createElement("div");
-// 					linkItem.className = "link-item";
-// 					const anchor = document.createElement("a");
-// 					anchor.href = skillData[key];
-// 					anchor.target = "_blank";
-// 					const paragraph = document.createElement("p");
-// 					paragraph.textContent = key;
-// 					anchor.appendChild(paragraph);
-// 					linkItem.appendChild(anchor);
-// 					container.appendChild(linkItem);
-// 				}
-// 			}
-// 		})
-// 		.catch((error) => console.error("Error fetching JSON:", error));
-// };
-
 function checkLinkStatus(link, statusId) {
     const statusCircle = document.getElementById(statusId);
 
